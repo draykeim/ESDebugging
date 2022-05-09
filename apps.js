c8a06315e96f21358e4d5276faa1b9ab20c421d3
devon <dkeim01@gmail.com>
2022-05-09 19:02:07 -0400

    Done finally

---
+++ apps.js
@@ -0,0 +1,22 @@
+const VarOne = 32;
+const VarTwo = 23;
+const add = (VarOne, VarTwo) => {
+    return VarOne + VarTwo
+}
+
+console.log(VarOne + VarTwo)
+
+const subtract = (VarOne, VarTwo) => {
+    return VarOne - VarTwo
+}
+console.log(VarOne - VarTwo)
+
+const multiply = (VarOne, VarTwo) => {
+ return VarOne * VarTwo
+}
+console.log(VarOne * VarTwo)
+
+const divide = (VarOne, VarTwo) => {
+return VarOne / VarTwo
+}
+console.log(VarOne / VarTwo)
